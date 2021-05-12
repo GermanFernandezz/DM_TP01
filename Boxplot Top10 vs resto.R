85c6ef953bca90d9aceb5d9f8d28522d141ae8c6
GermanFernandezz <83498105+GermanFernandezz@users.noreply.github.com>
2021-05-12 00:01:14 -0300

    Add files via upload

--- Boxplot Top10 vs resto.R
+++ Boxplot Top10 vs resto.R
@@ -8,8 +8,13 @@ features_avg_pos$top[features_avg_pos$mejorPosition == 1] <- "Top 1"
 features_avg_pos$top[features_avg_pos$mejorPosition > 1] <- "Top 2-10"
 features_avg_pos$top[features_avg_pos$mejorPosition > 10] <- "Top 11-200"
 
-pie(table(features_avg_pos$top),  main="Cantidad de temas por rango de posición")
+features_avg_pos_top1 <- features_avg_pos[features_avg_pos$top == 'Top 1', c(1:14)]
+features_avg_pos_top10 <- features_avg_pos[features_avg_pos$mejorPosition < 11, c(1:14)]
+features_avg_pos_top2_10 <- features_avg_pos[features_avg_pos$top == 'Top 2-10', c(1:14)]
 
+pie(table(features_avg_pos$top),  main="Cantidad de temas por rango de posición")
+dev.off()
+par(mfrow=c(1, 2), mar=c(5, 5, 5, 2) )
 boxplot(features_avg_pos$avg_danceability ~ features_avg_pos$top,
         col = c("blue", "red", "green"), 
         xlab = "Posicion en el chart", 
@@ -41,7 +46,7 @@ boxplot(features_avg_pos$avg_instrumentalness ~ features_avg_pos$top,
         xlab = "Posicion en el chart", 
         ylab = "Instrumentalness", 
         main = "Boxplot de Instrumentalness según posición en chart",
-        ylim = c(0,0.0000004))
+        ylim = c(0,1))
 
 boxplot(features_avg_pos$avg_acousticness ~ features_avg_pos$top,
         col = c("blue", "red", "green"),
@@ -78,16 +83,18 @@ boxplot(features_avg_pos$avg_tempo ~ features_avg_pos$top,
 
 
 library(modeest)
-means_TOP1 <- apply(features_avg_pos[features_avg_pos$top == "Top 1",c(4:13)], 2, mean)
-means_TOP10 <- apply(features_avg_pos[features_avg_pos$top == "Top 2-10",c(4:13)], 2, mean)
-means_TOP200 <- apply(features_avg_pos[features_avg_pos$top == "Top 11-200",c(4:13)], 2, mean)
-medians_TOP1 <- apply(features_avg_pos[features_avg_pos$top == "Top 1",c(4:13)], 2, median)
-medians_TOP10 <- apply(features_avg_pos[features_avg_pos$top == "Top 2-10",c(4:13)], 2, median)
-medians_TOP200 <- apply(features_avg_pos[features_avg_pos$top == "Top 11-200",c(4:13)], 2, median)
-sd_TOP1 <- apply(features_avg_pos[features_avg_pos$top == "Top 1",c(4:13)], 2, sd)
-sd_TOP10 <- apply(features_avg_pos[features_avg_pos$top == "Top 2-10",c(4:13)], 2, sd)
-sd_TOP200 <- apply(features_avg_pos[features_avg_pos$top == "Top 11-200",c(4:13)], 2, sd)
+means_TOP1 <- apply(features_avg_pos[features_avg_pos$top == "Top 1",c(4:11)], 2, mean)
+means_TOP10 <- apply(features_avg_pos[features_avg_pos$top == "Top 2-10",c(4:11)], 2, mean)
+means_TOP200 <- apply(features_avg_pos[features_avg_pos$top == "Top 11-200",c(4:11)], 2, mean)
+medians_TOP1 <- apply(features_avg_pos[features_avg_pos$top == "Top 1",c(4:11)], 2, median)
+medians_TOP10 <- apply(features_avg_pos[features_avg_pos$top == "Top 2-10",c(4:11)], 2, median)
+medians_TOP200 <- apply(features_avg_pos[features_avg_pos$top == "Top 11-200",c(4:11)], 2, median)
+sd_TOP1 <- apply(features_avg_pos[features_avg_pos$top == "Top 1",c(4:11)], 2, sd)
+sd_TOP10 <- apply(features_avg_pos[features_avg_pos$top == "Top 2-10",c(4:11)], 2, sd)
+sd_TOP200 <- apply(features_avg_pos[features_avg_pos$top == "Top 11-200",c(4:11)], 2, sd)
 
-medidas_tendencia_central <- data.frame(cbind(means_TOP1, means_TOP10, means_TOP200, medians_TOP1, medians_TOP10, medians_TOP200, sd_TOP1, sd_TOP10, sd_TOP200))
-names(medidas_tendencia_central) <- c("Promedio Top 1", "Promedio Top 2-10", "Promedio Top 11-200", "Mediana Top 1", "Mediana Top 2-10", "Mediana 11-200", "Des. Std. Top 1", "Des. Std. Top 2-10", "Des. Std. 11-200")
-rownames(medidas_tendencia_central) <- c("Danceability", "Energy", "Loudness", "Speechiness", "Acousticnes", "Instrumentalness", "Liveness", "Valence", "Tempo", "Duration")
+medidas_tendencia_central_porposicion <- data.frame(cbind(means_TOP1, means_TOP10, means_TOP200, medians_TOP1, medians_TOP10, medians_TOP200, sd_TOP1, sd_TOP10, sd_TOP200))
+names(medidas_tendencia_central_porposicion) <- c("Promedio Top 1", "Promedio Top 2-10", "Promedio Top 11-200", "Mediana Top 1", "Mediana Top 2-10", "Mediana 11-200", "Des. Std. Top 1", "Des. Std. Top 2-10", "Des. Std. 11-200")
+rownames(medidas_tendencia_central_porposicion) <- c("Danceability", "Energy", "Loudness", "Speechiness", "Acousticnes", "Instrumentalness", "Liveness", "Valence")
+
+
--- Histogramas - Scatters.R
+++ Histogramas - Scatters.R
@@ -11,12 +11,14 @@ features_avg_pos$top[features_avg_pos$mejorPosition > 10] <- "Top 11-200"
 
 par(mfrow=c(3, 1), mar=c(5, 10, 4, 2) )
 hist(features_avg_pos$avg_danceability, xlab="Danceability", main= "Histograma de Danceability")
-
+dev.off()
 par(mfrow=c(3, 1), mar=c(5, 10, 4, 2) )
 hist(features_avg_pos$avg_danceability[features_avg_pos$top == "Top 1"])
 hist(features_avg_pos$avg_danceability[features_avg_pos$top == "Top 2-10"])
 hist(features_avg_pos$avg_danceability[features_avg_pos$top == "Top 11-200"])
 
+par(mfrow=c(4, 2), mar=c(5, 10, 4, 2) )
+hist(features_avg_pos$avg_danceability, xlab="Danceability", main= "Histograma de Danceability")
 hist(features_avg_pos$avg_energy, xlab="Energy", main= "Histograma de Energy")
 hist(features_avg_pos$avg_loudness, xlab="Loudness", main= "Histograma de Loudness")
 hist(features_avg_pos$avg_speechiness, xlab="Speechiness", main= "Histograma de Speechiness")
@@ -27,16 +29,79 @@ hist(features_avg_pos$avg_valence, xlab="Valence", main= "Histograma de Valence"
 hist(features_avg_pos$avg_tempo, xlab="Tempo", main= "Histograma de Tempo")
 hist(features_avg_pos$avg_duration, xlab="Duration", main= "Histograma de Duration")
 
+
+media <- apply(features_avg_pos[,4:13], 2, mean)
+mediana <- apply(features_avg_pos[,4:13], 2, median)
+varianza <- apply(features_avg_pos[,4:13], 2, var)
+desvio <- apply(features_avg_pos[,4:13], 2, sd)
+medidas_tendencia_central <- data.frame(cbind(media, mediana, varianza, desvio))
+names(medidas_tendencia_central) <- c("Promedio Top 1", "Promedio Top 2-10", "Promedio Top 11-200", "Mediana Top 1", "Mediana Top 2-10", "Mediana 11-200", "Des. Std. Top 1", "Des. Std. Top 2-10", "Des. Std. 11-200")
+rownames(medidas_tendencia_central) <- c("Danceability", "Energy", "Loudness", "Speechiness", "Acousticnes", "Instrumentalness", "Liveness", "Valence", "Tempo", "Duration")
+
+
+
+
+plot(features_avg_pos$avg_danceability)
+plot(sort(features_avg_pos$avg_danceability))
+plot(features_avg_pos$avg_energy)
+plot(sort(features_avg_pos$avg_energy))
+
+
+
+
+
 dev.off()
-plot(features_avg_pos[4:13], labels = names(features_avg_pos)[4:13])
+plot(features_avg_pos[4:11], labels = names(features_avg_pos)[4:11])
+cor_top200 <- cor(features_avg_pos[,4:13])
+library(gplots)
+library(RColorBrewer)
+
+dev.off()
+par(mar=c(3,3,3,3))
+my_palette <- colorRampPalette(c("green", "black", "red"))(n = 100)
+heatmap.2(cor_top200,
+          cellnote = round(cor_top200,2), 
+          notecol="white",     
+          trace="none",        
+          margins = c(9,15), # 1er numero: margen inferior, 2do: margen izquierdo
+          col=my_palette,  
+          cexCol=0.75,
+          cexRow=0.75,
+          dendrogram="none",
+          symm= T,
+          Rowv=F) 
 
 features_avg_pos_top1 <- features_avg_pos[features_avg_pos$top == 'Top 1', c(1:14)]
 plot(features_avg_pos_top1[4:13], labels = names(features_avg_pos_top1)[4:13])
+cor_top1 <- cor(features_avg_pos_top1[,4:13])
+heatmap.2(cor_top1,
+          cellnote = round(cor_top1,2), 
+          notecol="white",     
+          trace="none",        
+          margins = c(9,15), # 1er numero: margen inferior, 2do: margen izquierdo
+          col=my_palette,  
+          cexCol=0.75,
+          cexRow=0.75,
+          dendrogram="none",
+          symm= T,
+          Rowv=F) 
 
-
-features_avg_pos_top10 <- features_avg_pos[features_avg_pos$top == 'Top 2-10', c(1:14)]
+features_avg_pos_top2_10 <- features_avg_pos[features_avg_pos$top == 'Top 2-10', c(1:14)]
 plot(features_avg_pos_top10[4:13], labels = names(features_avg_pos_top10)[4:13])
+cor_top2_10 <- cor(features_avg_pos_top2_10[,4:13])
+heatmap.2(cor_top2_10,
+          cellnote = round(cor_top2_10,2), 
+          notecol="white",     
+          trace="none",        
+          margins = c(9,15), # 1er numero: margen inferior, 2do: margen izquierdo
+          col=my_palette,  
+          cexCol=0.75,
+          cexRow=0.75,
+          dendrogram="none",
+          symm= T,
+          Rowv=F) 
 
+features_avg_pos_top10 <- features_avg_pos[features_avg_pos$mejorPosition < 11, c(1:14)]
 
 plot(features_avg_pos_top10$avg_energy, features_avg_pos_top10$avg_loudness)
 plot(features_avg_pos_top1$avg_energy, features_avg_pos_top1$avg_loudness)
